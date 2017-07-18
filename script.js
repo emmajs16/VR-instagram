38035429f544ad35794fa070036e35a124a49f3d
Emma Stoverink <emmajs16@live.com>
2017-07-18 21:45:45 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function levelOne(){
+    var view = document.getElementById("camera")
+    view.setAttribute("position", "0 1 -5")
+}
+function levelTwo(){
+    var view = document.getElementById("camera")
+    view.setAttribute("position", "0 1.5 -9")
+}
+function original(){
+    var view = document.getElementById("camera")
+    view.setAttribute("position","0 -0.5 0")
+}
+
